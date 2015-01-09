101c2fbde01ba36b1895d1b6d4969ba6d5fa3731
Paul McLanahan <pmac@mozilla.com>
2015-01-09 12:27:02 -0500

    Fix calls to management_cmd

--- bin/update/deploy_dev_base.py
+++ bin/update/deploy_dev_base.py
@@ -11,9 +11,9 @@ log = logging.getLogger(__name__)
 @task
 def database(ctx):
     # only ever run this one on demo and dev.
-    management_cmd('bedrock_truncate_database --yes-i-am-sure')
-    management_cmd('syncdb --migrate --noinput')
-    management_cmd('rnasync')
-    management_cmd('update_security_advisories --force --quiet')
-    management_cmd('cron update_reps_ical')
-    management_cmd('cron update_tweets')
+    management_cmd(ctx, 'bedrock_truncate_database --yes-i-am-sure')
+    management_cmd(ctx, 'syncdb --migrate --noinput')
+    management_cmd(ctx, 'rnasync')
+    management_cmd(ctx, 'update_security_advisories --force --quiet')
+    management_cmd(ctx, 'cron update_reps_ical')
+    management_cmd(ctx, 'cron update_tweets')
