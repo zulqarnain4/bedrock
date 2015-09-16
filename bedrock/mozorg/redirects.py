cbe96c5901912760140e62c31e5b7d862c386049
Paul McLanahan <pmac@mozilla.com>
2015-09-16 14:28:26 -0700

    Update redirects to make all trailing slashes optional.

--- bedrock/mozorg/redirects.py
+++ bedrock/mozorg/redirects.py
@@ -11,7 +11,7 @@ def to_uppercase(url):
 
 redirectpatterns = (
     # bug 755826
-    redirect('^zh-CN/$', 'http://firefox.com.cn/', locale_prefix=False),
+    redirect('^zh-CN/?$', 'http://firefox.com.cn/', locale_prefix=False),
 
     # bug 764261, 841393, 996608, 1008162, 1067691, 1113136, 1119022, 1131680, 1115626
     redirect('^zh-TW/?$', 'http://mozilla.com.tw/', locale_prefix=False),
@@ -35,7 +35,7 @@ redirectpatterns = (
     redirect('^firefox/xp', 'firefox.new'),
 
     # bug 857246 redirect /products/firefox/start/  to start.mozilla.org
-    redirect('^products/firefox/start/$', 'http://start.mozilla.org'),
+    redirect('^products/firefox/start/?$', 'http://start.mozilla.org'),
 
     # bug 875052
     redirect('^start/', ua_redirector('seamonkey',
@@ -125,41 +125,41 @@ redirectpatterns = (
              'https://wiki.mozilla.org/Webdev/GetInvolved/mozilla.org'),
 
     # Bug 763665, 1148127
-    redirect(r'^projects/$', 'firefox.family.index'),
+    redirect(r'^projects/?$', 'firefox.family.index'),
 
 
     # Bug 792185 Brand Toolkit -> Style Guide
-    redirect(r'^firefox/brand/$', 'styleguide.home'),
-    redirect(r'^firefox/brand/platform/$',
+    redirect(r'^firefox/brand/?$', 'styleguide.home'),
+    redirect(r'^firefox/brand/platform/?$',
              'styleguide.identity.firefox-family-platform'),
-    redirect(r'^firefox/brand/identity/$',
+    redirect(r'^firefox/brand/identity/?$',
              'styleguide.identity.firefox-branding'),
-    redirect(r'^firefox/brand/identity/channel-logos/$',
+    redirect(r'^firefox/brand/identity/channel-logos/?$',
              'styleguide.identity.firefox-channels'),
-    redirect(r'^firefox/brand/identity/wordmarks/$',
+    redirect(r'^firefox/brand/identity/wordmarks/?$',
              'styleguide.identity.firefox-wordmarks'),
-    redirect(r'^firefox/brand/identity/typefaces/$',
+    redirect(r'^firefox/brand/identity/typefaces/?$',
              'styleguide.communications.typefaces'),
-    redirect(r'^firefox/brand/artwork/$', 'styleguide.home'),
-    redirect(r'^firefox/brand/artwork/gear/$', 'styleguide.home'),
-    redirect(r'^firefox/brand/website/$',
+    redirect(r'^firefox/brand/artwork/?$', 'styleguide.home'),
+    redirect(r'^firefox/brand/artwork/gear/?$', 'styleguide.home'),
+    redirect(r'^firefox/brand/website/?$',
              'styleguide.websites.sandstone-intro'),
-    redirect(r'^firefox/brand/website/domain-strategy/$',
+    redirect(r'^firefox/brand/website/domain-strategy/?$',
              'styleguide.websites.domains-overview'),
-    redirect(r'^firefox/brand/copy/$', 'styleguide.communications.copy-tone'),
-    redirect(r'^firefox/brand/copy/l10n/$',
+    redirect(r'^firefox/brand/copy/?$', 'styleguide.communications.copy-tone'),
+    redirect(r'^firefox/brand/copy/l10n/?$',
              'styleguide.communications.translation'),
-    redirect(r'^firefox/brand/copy/rules/$',
+    redirect(r'^firefox/brand/copy/rules/?$',
              'styleguide.communications.copy-rules'),
-    redirect(r'^firefox/brand/downloads/$', 'styleguide.home'),
+    redirect(r'^firefox/brand/downloads/?$', 'styleguide.home'),
 
     # Bug 1186373
-    redirect(r'^firefox/hello/npssurvey/$',
+    redirect(r'^firefox/hello/npssurvey/?$',
              'https://www.surveygizmo.com/s3/2227372/Firefox-Hello-Product-Survey',
              permanent=False),
 
     # Bug 1071318
-    redirect(r'^firefox/mobile/$', 'firefox.android.index'),
+    redirect(r'^firefox/mobile/?$', 'firefox.android.index'),
 
     # Bug 804810 Identity Guidelines -> Style Guide
     redirect(r'^foundation/identity-guidelines/index.html', 'styleguide.home'),
@@ -170,18 +170,18 @@ redirectpatterns = (
 
     # Bug 945474 - delete Marketplace marketing product page
     # and redirect
-    redirect(r'^apps/$', 'https://marketplace.firefox.com/'),
+    redirect(r'^apps/?$', 'https://marketplace.firefox.com/'),
 
     # Bug 800467 /apps/partners ->
     # marketplace.firefox.com/developers
-    redirect(r'apps/partners/$',
+    redirect(r'apps/partners/?$',
              'https://marketplace.firefox.com/developers/'),
 
     # Bug 815527 /m/privacy.html -> /privacy/firefox/
     redirect(r'^m/privacy.html$', 'privacy.notices.firefox'),
 
     # Bug 1109318 /privacy/you -> privacy/tips/
-    redirect(r'^privacy/you/$',
+    redirect(r'^privacy/you/?$',
              'privacy.privacy-day'),
 
     # Bug 821047 /about/mission.html -> /mission/
@@ -192,41 +192,41 @@ redirectpatterns = (
 
     # Bug 800298 /webmaker/ -> wm.o and /webmaker/videos/ ->
     # wm.o/videos/
-    redirect(r'webmaker/$', 'https://webmaker.org'),
-    redirect(r'webmaker/videos/$', 'https://webmaker.org/videos/'),
+    redirect(r'webmaker/?$', 'https://webmaker.org'),
+    redirect(r'webmaker/videos/?$', 'https://webmaker.org/videos/'),
 
     # Bug 819317 /gameon/ -> gameon.m.o
-    redirect(r'gameon/$', 'https://gameon.mozilla.org'),
+    redirect(r'gameon/?$', 'https://gameon.mozilla.org'),
 
     # Bug 822817 /telemetry/ ->
     # https://wiki.mozilla.org/Telemetry/FAQ
-    redirect(r'telemetry/$', 'https://wiki.mozilla.org/Telemetry/FAQ'),
+    redirect(r'telemetry/?$', 'https://wiki.mozilla.org/Telemetry/FAQ'),
 
     # Bug 854561 - move /projects/mozilla-based/ to
     # /about/mozilla-based/
-    redirect(r'^projects/mozilla-based/$', '/about/mozilla-based/'),
+    redirect(r'^projects/mozilla-based/?$', '/about/mozilla-based/'),
 
     # Bug 867773 - Redirect the Persona "Developer FAQ" link
     # to MDN
-    redirect(r'^persona/developer-faq/$',
+    redirect(r'^persona/developer-faq/?$',
              'https://developer.mozilla.org/persona'),
 
     # Bug 981176 - For now we'll hard-code a redirect to 1.3
     # In the future this should automatically go to the
     # latest version's notes
-    redirect(r'^firefox/os/notes/$', '/firefox/os/notes/1.3/'),
+    redirect(r'^firefox/os/notes/?$', '/firefox/os/notes/1.3/'),
 
     # Bug 896585 - Send /contact/ to the spaces landing
-    redirect(r'^contact/$', '/contact/spaces/'),
+    redirect(r'^contact/?$', '/contact/spaces/'),
 
     # Bug 997577 - /legal/ -> /about/legal/
-    redirect(r'^legal/fraud-report/$', '/about/legal/fraud-report/'),
-    redirect(r'^legal/eula/$', '/about/legal/eula/'),
-    redirect(r'^legal/eula/firefox-2/$', '/about/legal/eula/firefox-2/'),
-    redirect(r'^legal/eula/firefox-3/$', '/about/legal/eula/firefox-3/'),
+    redirect(r'^legal/fraud-report/?$', '/about/legal/fraud-report/'),
+    redirect(r'^legal/eula/?$', '/about/legal/eula/'),
+    redirect(r'^legal/eula/firefox-2/?$', '/about/legal/eula/firefox-2/'),
+    redirect(r'^legal/eula/firefox-3/?$', '/about/legal/eula/firefox-3/'),
 
     # Bug 1073269 /dnt/ -> /firefox/dnt/
-    redirect(r'^dnt/$', 'firefox.dnt'),
+    redirect(r'^dnt/?$', 'firefox.dnt'),
 
     # bug 832348 **/index.html -> **/
     redirect('^(.*)/index.html$', '/{}/', locale_prefix=False),
